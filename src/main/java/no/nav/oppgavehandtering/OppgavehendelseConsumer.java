4500826eed1340bf1c523afc767a1b72571609bc
rindh <sjur.haugen.rindholen@nav.no>
2026-03-13 11:55:53 +0100

    ignorerer korrupte meldinger så lenge

--- src/main/java/no/nav/oppgavehandtering/OppgavehendelseConsumer.java
+++ src/main/java/no/nav/oppgavehandtering/OppgavehendelseConsumer.java
@@ -18,13 +18,8 @@ public class OppgavehendelseConsumer {
         if (oppgavehendelse.hendelse().hendelsestype() == Hendelsestype.OPPGAVE_ENDRET && oppgavehendelse.utfortAvMedarbeiderTildeltOppgaven()) {
             log.info("Oppgave: {}, Mottatt hendelse om endring utført av medarbeider som er tildelt oppgaven. Ikke relevant å varsle medarbeider", oppgavehendelse.oppgave().oppgaveId());
         }  else {
-            try {
-                log.info("Sender videre mottatt hendelse for: {}", oppgavehendelse.oppgave().oppgaveId());
-                oppgavehendelseMedarbeiderClient.sendNotifikasjon(oppgavehendelse);
-            } catch (Exception e) {
-                log.error("Kunne videresende hendelse for oppgave med id: {}", oppgavehendelse.oppgave().oppgaveId());
-                throw new IllegalArgumentException(e);
-            }
+            log.info("Sender videre mottatt hendelse for: {}", oppgavehendelse.oppgave().oppgaveId());
+            oppgavehendelseMedarbeiderClient.sendNotifikasjon(oppgavehendelse);
         }
     }
 }
